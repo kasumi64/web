37e908c258a5111dcb5b27770abb14aa99333a79
kasumi64 <77638292@qq.com>
2019-10-28 14:26:26 +0800

    react测试页面

---
+++ react/test/precache-manifest.54684b4e0f777a009b320a3b0eb6f2c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "335062c8fcf5442691b607c2f86ede8c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2b83fa3e519ccbb859d4",
+    "url": "./static/css/2.b30123e2.chunk.css"
+  },
+  {
+    "revision": "8c8ada72ec0e01b492a1",
+    "url": "./static/css/main.8a7fd8ff.chunk.css"
+  },
+  {
+    "revision": "2b83fa3e519ccbb859d4",
+    "url": "./static/js/2.e5f8c90d.chunk.js"
+  },
+  {
+    "revision": "8c8ada72ec0e01b492a1",
+    "url": "./static/js/main.9d29c79d.chunk.js"
+  },
+  {
+    "revision": "2b2a8dab9d23362ac093",
+    "url": "./static/js/runtime~main.e359d9d9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
